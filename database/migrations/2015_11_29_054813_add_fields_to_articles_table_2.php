d704f97a9235f29b6d2d972663cf3e6635fa673b
firmanfattah <firmanhidayat@outlook.com>
2015-11-30 04:50:09 +0700

    update

--- database/migrations/2015_11_29_054813_add_fields_to_articles_table_2.php
+++ database/migrations/2015_11_29_054813_add_fields_to_articles_table_2.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFieldsToArticlesTable extends Migration {
+class AddFieldsToArticlesTable2 extends Migration {
 
 	/**
 	 * Run the migrations.
